38bd343cd0e1370147de99f21a52ef7deadbd51f
LUCAS-VOSTRO\Lucas <lucas_marques73>
2017-03-10 15:37:11 -0300

    Aula 8 - Adicionando o Laravel Collective para criação de formulário e alterando o timezone para America/Sao_Paulo

--- aula01/laravel/config/app.php
+++ aula01/laravel/config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
@@ -179,6 +179,8 @@ return [
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -227,6 +229,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
